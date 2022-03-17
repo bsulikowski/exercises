29fd698bc03383c2a112a0d12165242bc24624ae
Ben Sulikowski <BenSulikowski@gmail.com>
2022-03-17 14:47:14 -0400

    Add more exercises

---
+++ rb101_rb109_small_problems/easy5/bannerizer.rb
@@ -0,0 +1,23 @@
+# Write a method that will take a short line of text, and print it within a box.
+
+# Example:
+# print_in_box('To boldly go where no one has gone before.')
+# +--------------------------------------------+
+# |                                            |
+# | To boldly go where no one has gone before. |
+# |                                            |
+# +--------------------------------------------+
+
+def print_in_box(message)
+  horizontal_rule = "+#{'-' * (message.size + 2)}+"
+  empty_line = "|#{' ' * (message.size + 2)}|"
+
+  puts horizontal_rule
+  puts empty_line
+  puts "| #{message} |"
+  puts empty_line
+  puts horizontal_rule
+end
+
+print_in_box('')
+print_in_box('To boldly go where no one has gone before.')
---
+++ rb101_rb109_small_problems/easy5/spin_me_around_in_circles.rb
@@ -0,0 +1,14 @@
+# You are given a method named spin_me that takes a string as an argument and returns
+# a string that contains the same words, but with each word's characters reversed. Given
+# the method's implementation, will the returned string be the same object as the one
+# passed in as an argument or a different object?
+
+def spin_me(str)
+  str.split.each do |word|
+    word.reverse!
+  end.join(" ")
+end
+
+spin_me("hello world") # "olleh dlrow"
+
+# Returned string will be a new object since we used split to convert the string to an array
